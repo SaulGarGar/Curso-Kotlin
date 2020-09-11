e52db2056181665e492381c0969aa2380125e33f
saul.garcia <saul.garcia@kavak.com>
2020-09-10 21:15:32 -0500

    Add session 1.

---
+++ Sesion1Conceptos.kt
@@ -0,0 +1,32 @@
+/*
+Lógica: Método o razonamiento en el que las ideas o la sucesión de los hechos 
+se manifiestan o se desarrollan de forma coherente y sin que haya contradicciones entre ellas.
+
+Programa informático:  Secuencia de instrucciones, escritas para realizar una tarea específica en una computadora.
+
+Lenguaje: Es un sistema de comunicación estructurado para el que existe un contexto de uso y ciertos principios combinatorios
+formales. En nuestro caso será la herramienta que nos permita darle indicaciones a la computadora.
+
+Nivel de abstraccion: Es el nivel de complejidad que tiene un lenguaje comparado con el lenguaje humano. 
+Entre mas abstracto sea el lenguaje de programación, será mas parecido al lenguaje humano 
+y entre menos abstracto sea, será mas parecido al lenguaje máquina.
+
+Algoritmo: es un conjunto de instrucciones o reglas definidas y no-ambiguas, ordenadas y finitas que permite realizar una tarea o
+solucionar un problema, 
+*/
+
+//Una variable es un contenedor de datos, cuenta con un nombre y un valor                      
+    var numero = 5
+
+
+//Declaración de una variable
+//Existen variables y constantes
+//Las variables se declaran usando la palabra reservada "var" y pueden cambiar su valor n veces durante el tiempo de ejecución.
+    var nombreVariable = "Hector"
+
+//Las constantes se declaran usando la palabra reservada "val" y el primer valor que se le asigna será su valor definitivo. NO PUEDE SER CAMBIADO
+    val nombreConstante = "Carlos"
+ 
+
+
+ 
---
+++ Sesion2Datos.kt
@@ -0,0 +1,15 @@
+//Tipos de datos
+//String   conjunto de caracteres
+    var dirección: String = "Av Centenario 3008"
+
+//Int  númerico entero
+    var edad: Int = 27
+
+//Float  numero con máximo 6 decimales
+    var estatura: Float = 1.74
+
+//Double numero con máximo 15 decimales 
+    val pi: Double = 3.14159265359
+
+//Boolean  afirmación o negación
+    val laTvEstaPrendida = false
